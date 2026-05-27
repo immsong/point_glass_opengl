eb3858cd052e0578363aea62868bfca1ee0689ce
Mark Song <immsong@icloud.com>
2026-05-27 17:46:40 +0900

    test: verify rust core connection via ffi returning 42

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:point_glass_opengl/point_glass_opengl.dart' as point_glass_opengl;
+import 'package:point_glass_opengl/point_glass_opengl.dart'
+    as point_glass_opengl;
 
 void main() {
   runApp(const MyApp());
@@ -15,56 +16,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late int sumResult;
-  late Future<int> sumAsyncResult;
-
   @override
   void initState() {
     super.initState();
-    sumResult = point_glass_opengl.sum(1, 2);
-    sumAsyncResult = point_glass_opengl.sumAsync(3, 4);
   }
 
   @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Native Packages')),
         body: SingleChildScrollView(
           child: Container(
-            padding: const .all(10),
-            child: Column(
-              children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: .center,
-                ),
-                spacerSmall,
-                Text(
-                  'sum(1, 2) = $sumResult',
-                  style: textStyle,
-                  textAlign: .center,
-                ),
-                spacerSmall,
-                FutureBuilder<int>(
-                  future: sumAsyncResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue = (value.hasData)
-                        ? value.data
-                        : 'loading';
-                    return Text(
-                      'await sumAsync(3, 4) = $displayValue',
-                      style: textStyle,
-                      textAlign: .center,
-                    );
-                  },
-                ),
-              ],
-            ),
+            padding: EdgeInsets.all(10),
+            child: Column(children: []),
           ),
         ),
       ),
